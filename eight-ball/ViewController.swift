d99e238d3c7ee9b2500615c48b07cc20a6684c7b
Rwoltx <richardx1016@gmail.com>
2018-08-28 13:20:58 -0400

    Finished eight-bll functionality

--- eight-ball/ViewController.swift
+++ eight-ball/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    var answers = ["yes","No","Maybe","Nope","Sure"]
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func flipEightBall(_ sender: Any) {
+        
+        let index = Int(arc4random_uniform(UInt32(answers.count)))
+        
+        let answer = answers[index]
+        
+        answerLabel.text = answer
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
